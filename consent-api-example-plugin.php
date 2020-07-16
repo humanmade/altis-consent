2b08bad46676064769e898b74e1cf72583c59ddd
Chris Reynolds <chris@jazzsequence.com>
2020-07-16 13:09:30 -0600

    add files (and modified files) from copied demo plugin

---
+++ consent-api-example-plugin.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Plugin Name: Example plugin for the WP Consent Level API
+ * Plugin URI: https://www.wordpress.org/plugins/wp-consent-api
+ * Description: Example plugin to demonstrate usage of the Consent API
+ * Version: 1.0.0
+ * Text Domain: wp-consent-api
+ * Domain Path: /languages
+ * Author: WP privacy team
+ * Author URI:
+ */
+
+require_once __DIR__ . '/settings.php';
+
+$plugin_data = get_file_data( __FILE__, array( 'Version' => 'Version' ), false );
+define( 'CONSENT_API_EXAMPLE_PLUGIN_VERSION', $plugin_data['Version'] );$plugin = plugin_basename(__FILE__);
+
+/**
+ * Tell the consent API we're following the api
+ */
+add_filter("wp_consent_api_registered_$plugin", function(){return true;});
+
+add_action( 'wp_enqueue_scripts', 'example_plugin_enqueue_assets' );
+function example_plugin_enqueue_assets( $hook ) {
+	wp_enqueue_script( 'example-plugin', plugin_dir_url(__FILE__) . "main.js", array('jquery'), CONSENT_API_EXAMPLE_PLUGIN_VERSION, true );
+}
+
+add_shortcode('example-plugin-shortcode', 'example_plugin_load_document');
+function example_plugin_load_document($atts = [], $content = null, $tag = '')
+{
+	$atts = array_change_key_case((array)$atts, CASE_LOWER);
+	ob_start();
+
+	// override default attributes with user attributes
+	$atts = shortcode_atts(array('category' => 'marketing'), $atts, $tag);
+	//default
+	$category = 'marketing';
+	if (function_exists('wp_validate_consent_category')){
+		$category = wp_validate_consent_category($atts['category']);
+    }
+
+	?>
+	<div id="example-plugin-content" data-consentcategory="<?php echo $category?>">
+		<div class="functional-content">
+			<h1>No consent has been given yet for category <?php echo $category?>. </h1>
+			<button class="give-consent">Give consent for <?php echo $category; ?></button>
+		</div>
+		<div class="marketing-content" style="display:none">
+			<h1>Woohoo! consent has been given for category <?php echo $category?> :)</h1>
+			<button class="revoke-consent">Revoke consent for <?php echo $category; ?></button>
+		</div>
+
+	</div>
+
+	<?php
+	return ob_get_clean();
+}
+
+add_filter( 'wp_get_consent_type', function() {
+	return 'optin';
+}, 10 );
---
+++ settings.php
@@ -0,0 +1,147 @@
+<?php
+
+function bootstrap() {
+	add_action( 'admin_init', 'register_consent_settings', 99 );
+	add_action( 'admin_menu', 'add_altis_privacy_page' );
+}
+
+function add_altis_privacy_page() {
+	add_options_page(
+		'Privacy',
+		'Cookies',
+		'manage_options',
+		'altis_privacy',
+		'render_altis_privacy_page'
+	);
+}
+
+function get_cookie_consent_settings_fields() {
+	$fields = [
+		[
+			'id'       => 'cookie_expiration',
+			'title'    => __( 'Cookie Expiration', 'altis-consent' ),
+			'callback' => 'cookie_expiration',
+		],
+		[
+			'id'       => 'banner_options',
+			'title'    => __( 'Consent Banner Options', 'altis-consent' ),
+			'callback' => 'render_banner_options',
+		],
+		[
+			'id' => 'banner_text',
+			'title' => __( 'Banner Message', 'altis-consent' ),
+			'callback' => 'render_banner_message',
+		],
+		[
+			'id' => 'cookie_policy_page',
+			'title' => __( 'Cookie Policy Page', 'altis-consent' ),
+			'callback' => 'render_cookie_policy_page',
+		],
+	];
+
+	return apply_filters( 'altis.consent.consent_settings_fields', $fields );
+}
+
+function register_consent_settings() {
+	$page    = 'altis_privacy';
+	$section = 'cookie_consent';
+
+	register_setting( 'cookie_consent_options', 'cookie_consent_options', 'validate_some_stuff' );
+
+	add_settings_section(
+		$section,                                // New settings section
+		__( 'Cookie Consent', 'altis-consent' ), // Section title
+		'cookie_settings_page',                  // Callback function
+		$page                                    // Settings Page.
+
+	);
+
+	$fields = get_cookie_consent_settings_fields();
+
+	foreach ( $fields as $field ) {
+		add_settings_field( $field['id'], $field['title'], $field['callback'], $page, $section );
+	}
+}
+
+
+function validate_some_stuff( $stuff ) {
+	return $stuff;
+}
+
+function render_altis_privacy_page() {
+	?>
+	<h1>Cookie Settings</h1>
+	<form action="options.php" method="post">
+		<?php
+		settings_fields( 'cookie_consent_options' );
+		do_settings_sections( 'altis_privacy' );
+		?>
+		<input name="submit" class="button button-primary" type="submit" value="<?php esc_attr_e( 'Save' ); ?>" />
+	</form>
+	<?php
+}
+
+function cookie_settings_page() {
+}
+
+function cookie_expiration() {
+	$options = get_option( 'cookie_consent_options' );
+	$expiration = ! empty( $options['cookie_expiration'] ) ? $options['cookie_expiration'] : 14;
+	?>
+	<input id="cookie_consent_expiration" name="cookie_consent_options[cookie_expiration]" type="number" value="<?php echo absint( $expiration ); ?>" />
+	<p class="description">
+		How long, in days, cookies should be stored.
+	</p>
+	<?php
+}
+
+function render_banner_options() {
+	$options  = get_option( 'cookie_consent_options' );
+	$selected = $options['banner_options'] ?: '';
+	?>
+	<select name="cookie_consent_options[banner_options]" id="banner_options" value="<?php echo esc_attr( $selected ); ?>">
+		<option value="" <?php selected( $selected, '' ); ?>>&mdash; Please select an option &mdash;</option>
+		<option value="all-categories" <?php selected( $selected, 'all-categories' ); ?>>All Cookie Categories</option>
+		<option value="none" <?php selected( $selected, 'none' ); ?>>Allow/Deny All Cookies</option>
+	</select>
+	<p class="description">
+		If you would like to display an option to configure each cookie category in the consent banner, select All Cookie Categories.<br />
+		If you would like to only display an option to accept or deny all non-functional cookies, select Allow/Deny All Cookies.
+	</p>
+	<?php
+}
+
+function render_banner_message() {
+	$options = get_option( 'cookie_consent_options' );
+	$message = $options['banner_message'] ?: 'This site uses cookies to provide a better user experience.';
+
+	wp_editor( wp_kses_post( $message ), 'banner_message', [
+		'textarea_name' => 'cookie_consent_options[banner_message]',
+		'teeny'         => true,
+		'textarea_rows' => 5,
+	] );
+}
+
+function render_cookie_policy_page() {
+	$options = get_option( 'cookie_consent_options' );
+	$page_id = $options['policy_page'] ?: 0;
+	$has_pages = (bool) get_posts( [
+		'post_type'      => 'page',
+		'posts_per_page' => 1,
+		'post_status'    => [ 'publish', 'draft' ],
+	] );
+
+	if ( $has_pages ) {
+		wp_dropdown_pages( [
+			'name' => 'cookie_consent_options[policy_page]',
+			'show_option_none' => '&mdash; Select an option &mdash;',
+			'option_none_value' => '0',
+			'selected'          => $page_id,
+			'post_status'       => [ 'draft', 'publish' ],
+		] );
+	} else {
+		esc_html_e( 'There are no pages.' );
+	}
+}
+
+bootstrap();
