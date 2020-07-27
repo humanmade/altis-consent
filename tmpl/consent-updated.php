2dd3a8d2bfc9e160eb6663c7f8f0285851fcf98f
Chris Reynolds <chris@jazzsequence.com>
2020-07-27 15:57:53 -0600

    add a new consent-updated message
    with the current setup this just disappears, will need to do something about that

---
+++ tmpl/consent-updated.php
@@ -0,0 +1,8 @@
+<div class="consent-updated-message">
+	<p class="message">
+		<span class="preferences-updated-message">
+			<?php echo wp_kses_post( apply_filters( 'altis.consent.preferences_updated_message', __( 'Cookie preferences updated.', 'altis-consent' ) ) ); ?>
+		</span>
+		<button class="close-message"><span class="dashicons dashicons-no-alt"></span></button>
+	</p>
+</div>
