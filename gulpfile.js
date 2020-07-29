78c93c16fdc7861496c7c0ca0014c7180542cd34
Chris Reynolds <chris@jazzsequence.com>
2020-07-29 14:37:00 -0600

    update config to add minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,43 @@
-const gulp = require( 'gulp' ),
+const csso = require( 'gulp-csso' ),
+	  del = require( 'del' ),
+	  gulp = require( 'gulp' ),
 	  sass = require( 'gulp-sass' ),
-	  del = require( 'del' );
+	  terser = require( 'gulp-terser' );
 
 gulp.task( 'styles', () => {
 	return gulp.src( 'assets/sass/**/*.scss' )
 		.pipe( sass().on( 'error', sass.logError ) )
-		.pipe( gulp.dest( 'assets/css/' ) );
+		.pipe( csso() )
+		.pipe( gulp.dest( 'dist/css/' ) );
 } );
 
-gulp.task( 'clean', () => {
+gulp.task( 'scripts', () => {
+	return gulp.src( 'assets/js/**/*.js' )
+		.pipe( terser() )
+		.pipe( gulp.dest( 'dist/js' ) );
+} );
+
+gulp.task( 'clean:css', () => {
 	return del( [
-		'assets/css/style.css',
+		'dist/css/style.css',
 	] );
 } );
 
-gulp.task( 'build', gulp.series( [ 'clean', 'styles' ] ) );
+gulp.task( 'clean:js', () => {
+	return del( [
+		'dist/js/main.js',
+	] );
+} );
+
+gulp.task( 'clean', gulp.series( [ 'clean:css', 'clean:js' ] ) );
+gulp.task( 'build', gulp.series( [ 'clean', 'scripts', 'styles' ] ) );
 
 gulp.task( 'watch', () => {
 	gulp.watch( 'assets/sass/**/*.scss', done => {
-		gulp.series( [ 'clean', 'styles' ] )( done );
+		gulp.series( [ 'clean:css', 'styles' ] )( done );
+	} );
+
+	gulp.watch( 'assets/js/**/*.js', done => {
+		gulp.series( [ 'clean:js', 'scripts' ] )( done );
 	} );
 } );
