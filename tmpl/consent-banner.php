53945beb3a40a0afb7108c90d2e117765841f80f
Chris Reynolds <chris@jazzsequence.com>
2020-08-04 15:23:44 -0600

    move the updated banner outside of the main consent banner

--- tmpl/consent-banner.php
+++ tmpl/consent-banner.php
@@ -8,21 +8,23 @@ $no_option_saved_message = sprintf(
 );
 ?>
 
-<div id="cookie-consent-banner" data-consentcategory="all" class="consent-banner">
-	<?php
-	if ( '' === $options['banner_options'] ) {
-		echo wp_kses_post(
-			/**
-			 * Allow the no option saved message to be filtered.
-			 *
-			 * @var string $no_option_saved_message The message to output when no consent option has been saved.
-			 */
-			apply_filters( 'altis.consent.no_option_saved_message', $no_option_saved_message )
-		);
-	} else {
-		load_template( __DIR__ . '/consent-updated.php' );
-		load_template( __DIR__ . '/cookie-preferences.php' );
-		load_template( __DIR__ . '/button-row.php' );
-	}
-	?>
+<div id="cookie-consent-banner">
+	<?php load_template( __DIR__ . '/consent-updated.php' ); ?>
+	<div class="consent-banner">
+		<?php
+		if ( '' === $options['banner_options'] ) {
+			echo wp_kses_post(
+				/**
+				 * Allow the no option saved message to be filtered.
+				 *
+				 * @var string $no_option_saved_message The message to output when no consent option has been saved.
+				 */
+				apply_filters( 'altis.consent.no_option_saved_message', $no_option_saved_message )
+			);
+		} else {
+			load_template( __DIR__ . '/cookie-preferences.php' );
+			load_template( __DIR__ . '/button-row.php' );
+		}
+		?>
+	</div>
 </div>
