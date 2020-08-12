e9c8cc6ddd1ee85c79a1fe54a49dab5f9aef7731
Chris Reynolds <chris@jazzsequence.com>
2020-08-12 11:43:37 -0600

    allow all the template paths to be filtered
    this will allow themes/plugins to define their own templates while still using the overall template architecture for other templates.

--- tmpl/consent-banner.php
+++ tmpl/consent-banner.php
@@ -9,7 +9,14 @@ $no_option_saved_message = sprintf(
 ?>
 
 <div id="cookie-consent-banner">
-	<?php load_template( __DIR__ . '/consent-updated.php' ); ?>
+	<?php
+	/**
+	 * Allow the consent updated template path to be overridden so it can be customized independently. This template displays the "preferences updated" messaging after they have been saved.
+	 *
+	 * @var string The path to the consent updated template.
+	 */
+	apply_filters( 'altis.consent.consent_updated_template_path', load_template( __DIR__ . '/consent-updated.php' ) );
+	?>
 	<div class="consent-banner">
 		<?php
 		if ( '' === $options['banner_options'] ) {
@@ -22,8 +29,19 @@ $no_option_saved_message = sprintf(
 				apply_filters( 'altis.consent.no_option_saved_message', $no_option_saved_message )
 			);
 		} else {
-			load_template( __DIR__ . '/cookie-preferences.php' );
-			load_template( __DIR__ . '/button-row.php' );
+			/**
+			 * Allow the cookie preferences template path to be overridden so it can be customized independently.
+			 *
+			 * @var string The path to the cookie preferences template.
+			 */
+			apply_filters( 'altis.consent.cookie_preferences_template_path', load_template( __DIR__ . '/cookie-preferences.php' ) );
+
+			/**
+			 * Allow the button row template path to be overridden so it can be customized independently. This is the main content template that includes the buttons and the messaging of the banner.
+			 *
+			 * @var string The path to the button row template.
+			 */
+			apply_filters( 'altis.consent.button_row_template_path', load_template( __DIR__ . '/button-row.php' ) );
 		}
 		?>
 	</div>
