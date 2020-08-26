7bbc4714a546146689e58d163f7f7f7c7b13eaaf
Chris Reynolds <chris@jazzsequence.com>
2020-08-26 09:38:19 -0600

    drop the else and just set the default url to be an empty string

--- inc/functions.php
+++ inc/functions.php
@@ -149,13 +149,12 @@ function should_display_banner() : bool {
  * @return string The cookie policy page url.
  */
 function get_cookie_policy_url() : string {
-	$cookie_policy_page_id = (int) Settings\get_consent_option( 'policy_page', 0 );
+	$cookie_policy_page_url = '';
+	$cookie_policy_page_id  = (int) Settings\get_consent_option( 'policy_page', 0 );
 
 	// Make sure the cookie policy page ID is an actual page.
 	if ( 'page' === get_post_type( $cookie_policy_page_id ) ) {
 		$cookie_policy_page_url = get_page_uri( $cookie_policy_page_id );
-	} else {
-		$cookie_policy_page_url = '';
 	}
 
 	/**
