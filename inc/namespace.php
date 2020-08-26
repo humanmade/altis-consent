5d652f3b53ce1645c44b9887b58aecec5f5cdac9
Chris Reynolds <chris@jazzsequence.com>
2020-08-26 12:02:53 -0600

    remove jquery dependency

--- inc/namespace.php
+++ inc/namespace.php
@@ -5,6 +5,9 @@ namespace Altis\Consent;
 use Altis;
 use WP_CONSENT_API;
 
+/**
+ * Kick everything off.
+ */
 function bootstrap() {
 	// Register this plugin with the consent API.
 	add_filter( 'wp_consent_api_registered_' . plugin_basename( __FILE__ ), '__return_true' );
@@ -17,20 +20,28 @@ function bootstrap() {
 	// Enqueue the javascript handler.
 	add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_assets' );
 
-	// Shortcode. Replace with an actual way to display a banner.
+	// Shortcode. Not recommended but here in case it's needed.
 	add_shortcode( 'cookie-consent-banner', __NAMESPACE__ . '\\render_consent_banner' );
 }
 
+/**
+ * Enqueue css and js.
+ */
 function enqueue_assets() {
-	$js = plugin_dir_url( __DIR__ ) . 'dist/js/main.js';
+	$js  = plugin_dir_url( __DIR__ ) . 'dist/js/main.js';
+	$css = plugin_dir_url( __DIR__ ) . 'dist/css/styles.css';
+	$ver = '1.0.0';
 
-	// If working locally, load the unminified version of the js file.
 	if ( Altis\get_environment_type() === 'local' ) {
-		$js = plugin_dir_url( __DIR__ ) . 'assets/js/main.js';
+		// If working locally, load the unminified version of the js file.
+		$js   = plugin_dir_url( __DIR__ ) . 'assets/js/main.js';
+
+		// Break the cache on local.
+		$ver .= '-' . filemtime( $css );
 	}
 
-	wp_enqueue_script( 'altis-consent', $js, [ 'jquery' ], '0.0.1', true );
-	wp_enqueue_style( 'altis-consent', plugin_dir_url( __DIR__ ) . 'dist/css/styles.css', [], '0.0.1-' . time(), 'screen' );
+	wp_enqueue_script( 'altis-consent', $js, [], $ver, true );
+	wp_enqueue_style( 'altis-consent', $css, [], $ver, 'screen' );
 
 	wp_localize_script( 'altis-consent', 'altisConsent', [
 		'categories' => WP_CONSENT_API::$config->consent_categories(),
@@ -43,6 +54,13 @@ function enqueue_assets() {
 	] );
 }
 
+/**
+ * Output the consent banner.
+ *
+ * Output here is returned rather than explicitly loaded in case it needs to be loaded into a variable.
+ *
+ * @return string The consent banner html.
+ */
 function render_consent_banner() : string {
 	ob_start();
 	load_consent_banner();
