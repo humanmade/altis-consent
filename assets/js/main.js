1178a2494c9d2c122a5d196a217981039c171a2f
Chris Reynolds <chris@jazzsequence.com>
2020-11-19 13:30:47 -0700

    namespace all the things

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,16 +6,17 @@
  */
 
 let Altis = window.Altis || {};
-Altis.Consent = {};
 
-const giveConsentButton  = document.querySelector( '.give-consent' ),
-	revokeConsentButton  = document.querySelector( '.revoke-consent' ),
-	cookiePreferences    = document.querySelector( '.cookie-preferences' ).classList,
-	cookiePrefsButton    = document.querySelector( '.view-preferences' ),
-	applyCookiePrefs     = document.querySelector( '.apply-cookie-preferences' ),
-	cookieUpdatedMessage = document.querySelector( '.consent-updated-message' ).classList,
-	closeUpdatedMessage  = document.getElementById( 'consent-close-updated-message' ),
-	consentBanner        = document.getElementById( 'cookie-consent-banner' );
+Altis.Consent = {
+	giveConsentButton: document.querySelector( '.give-consent' ),
+	revokeConsentButton: document.querySelector( '.revoke-consent' ),
+	cookiePreferences: document.querySelector( '.cookie-preferences' ).classList,
+	cookiePrefsButton: document.querySelector( '.view-preferences' ),
+	applyCookiePrefs: document.querySelector( '.apply-cookie-preferences' ),
+	cookieUpdatedMessage: document.querySelector( '.consent-updated-message' ).classList,
+	closeUpdatedMessage: document.getElementById( 'consent-close-updated-message' ),
+	consentBanner: document.getElementById( 'cookie-consent-banner' ),
+};
 
 /**
  * Check if a user has given consent for a specific category.
@@ -24,9 +25,9 @@ const giveConsentButton  = document.querySelector( '.give-consent' ),
  *
  * @param {string} category The category to check consent against.
  */
-function hasConsent( category ) {
+Altis.Consent.has = function ( category ) {
 	return wp_has_consent( category );
-}
+};
 
 /**
  * Set a new consent category value.
@@ -36,9 +37,9 @@ function hasConsent( category ) {
  * @param {string} category The consent category to update.
  * @param {string} value The value to update the consent category to.
  */
-function setConsent( category, value ) { // eslint-disable-line no-unused-vars
+Altis.Consent.set = function ( category, value ) {
 	wp_set_consent( category, value );
-}
+};
 
 /**
  * Set cookie by consent type.
@@ -48,9 +49,9 @@ function setConsent( category, value ) { // eslint-disable-line no-unused-vars
  * @param {string} name The cookie name to set.
  * @param {string} value The cookie value to set.
  */
-function setCookie( name, value ) { // eslint-disable-line no-unused-vars
+Altis.Consent.setCookie = function ( name, value ) { // eslint-disable-line no-unused-vars
 	consent_api_set_cookie( name, value );
-}
+};
 
 /**
  * Retrieve a cookie by name.
@@ -59,23 +60,23 @@ function setCookie( name, value ) { // eslint-disable-line no-unused-vars
  *
  * @param {string} name The name of the cookie to get data from.
  */
-function getCookie( name ) { // eslint-disable-line no-unused-vars
+Altis.Consent.getCookie = function ( name ) { // eslint-disable-line no-unused-vars
 	return consent_api_get_cookie( name );
-}
+};
 
 /**
  * Check if a consent cookie has already been saved on the client machine.
  *
  * @return {bool} Return true if consent has been given previously.
  */
-function consentCookieSaved() {
+Altis.Consent.cookieSaved = function () {
 	let consentExists = false;
 	altisConsent.categories
 		// Skip the functional cookies preference.
 		.filter( category => category === 'functional' )
 		// Loop through the rest of the categories.
 		.forEach( category => {
-			if ( hasConsent( category ) ) {
+			if ( Altis.Consent.get( category ) ) {
 				consentExists = true;
 			}
 
@@ -83,14 +84,14 @@ function consentCookieSaved() {
 		} );
 
 	return consentExists;
-}
+};
 
 /**
  * Return an array of all the categories that a user has consented to.
  *
  * @return {array} An array of allowed cookie categories.
  */
-function getConsentedCategories() { // eslint-disable-line no-unused-vars
+Altis.Consent.getCategories = function () { // eslint-disable-line no-unused-vars
 	// Start off with the allowlisted categories.
 	let hasConsent = altisConsent.alwaysAllowCategories;
 
@@ -101,7 +102,7 @@ function getConsentedCategories() { // eslint-disable-line no-unused-vars
 	} );
 
 	return hasConsent;
-}
+};
 
 /**
  * Update consent for individual categories.
@@ -112,7 +113,7 @@ Altis.Consent.updateCategories = function () {
 		unselected = [];
 
 	// If we're selecting categories from inputs, add the selected categories to an array and the unselected categories to a different array.
-	if ( cookiePreferences && cookiePreferences.contains( 'show' ) ) {
+	if ( Altis.Consent.cookiePreferences && Altis.Consent.cookiePreferences.contains( 'show' ) ) {
 		for ( const category of categories ) {
 			if ( category.checked ) {
 				selected.push( category.value );
@@ -147,36 +148,36 @@ Altis.Consent.updateCategories = function () {
 
 	// Set the allowed categories.
 	for ( const selectedCategory of selected ) {
-		wp_set_consent( selectedCategory, 'allow' );
+		Altis.Consent.set( selectedCategory, 'allow' );
 	}
 
 	// Set the disallowed categories.
 	for ( const unselectedCategory of unselected ) {
-		wp_set_consent( unselectedCategory, 'deny' );
+		Altis.Consent.set( unselectedCategory, 'deny' );
 	}
 
 	// Toggle the cookie preferences if we've passed specific categories.
-	if ( cookiePreferences && cookiePreferences.contains( 'show' ) ) {
-		cookiePreferences.remove( 'show' );
+	if ( Altis.Consent.cookiePreferences && Altis.Consent.cookiePreferences.contains( 'show' ) ) {
+		Altis.Consent.cookiePreferences.remove( 'show' );
 
 		// Show the buttons if they are hidden.
-		giveConsentButton.classList.remove( 'hide' );
-		revokeConsentButton.classList.remove( 'hide' );
+		Altis.Consent.giveConsentButton.classList.remove( 'hide' );
+		Altis.Consent.revokeConsentButton.classList.remove( 'hide' );
 	}
 
 	document.querySelector( '.consent-banner' ).classList.add( 'hide' );
 
-	preferencesUpdatedMessage();
+	Altis.Consent.preferencesUpdatedMessage();
 };
 
 /**
  * Show or hide the cookie preferences.
  */
 Altis.Consent.toggleCookiePrefs = function () {
-	const allowAllClasses      = giveConsentButton.classList,
-		allowFunctionalClasses = revokeConsentButton.classList;
+	const allowAllClasses      = Altis.Consent.giveConsentButton.classList,
+		allowFunctionalClasses = Altis.Consent.revokeConsentButton.classList;
 
-	cookiePreferences.toggle( 'show' );
+	Altis.Consent.cookiePreferences.toggle( 'show' );
 
 	// Toggle the other buttons when we show the cookie prefs.
 	allowAllClasses.toggle( 'hide' );
@@ -186,32 +187,32 @@ Altis.Consent.toggleCookiePrefs = function () {
 /**
  * Check if consent has been given already. If not, toggle display of the banner.
  */
-function maybeDisplayBanner() {
-	if ( ! consentCookieSaved() && altisConsent.shouldDisplayBanner ) {
-		consentBanner.style.display = 'block';
+Altis.Consent.maybeDisplayBanner = function () {
+	if ( ! Altis.Consent.consentCookieSaved() && altisConsent.shouldDisplayBanner ) {
+		Altis.Consent.consentBanner.style.display = 'block';
 	}
-}
+};
 
 /**
  * Show the preferences updated message.
  */
-function preferencesUpdatedMessage() {
+Altis.Consent.preferencesUpdatedMessage = function () {
 	const consentUpdated = document.querySelector( '.consent-updated-message' ).classList;
 	consentUpdated.toggle( 'show' );
-}
+};
 
 // Display the banner. Or not.
-maybeDisplayBanner();
+Altis.Consent.maybeDisplayBanner();
 
 // Toggle consent when grant/revoke consent button is clicked.
-giveConsentButton.addEventListener( 'click', Altis.Consent.updateCategories );
-revokeConsentButton.addEventListener( 'click', Altis.Consent.updateCategories );
+Altis.Consent.giveConsentButton.addEventListener( 'click', Altis.Consent.updateCategories );
+Altis.Consent.revokeConsentButton.addEventListener( 'click', Altis.Consent.updateCategories );
 
 // Make sure the preverences button exists before triggering an on-click action.
-if ( cookiePrefsButton ) {
-	cookiePrefsButton.addEventListener( 'click', Altis.Consent.toggleCookiePrefs );
-	applyCookiePrefs.addEventListener( 'click', Altis.Consent.updateCategories );
+if ( Altis.Consent.cookiePrefsButton ) {
+	Altis.Consent.cookiePrefsButton.addEventListener( 'click', Altis.Consent.toggleCookiePrefs );
+	Altis.Consent.applyCookiePrefs.addEventListener( 'click', Altis.Consent.updateCategories );
 }
 
 // Close the banner if the close button is clicked.
-closeUpdatedMessage.addEventListener( 'click', () => cookieUpdatedMessage.toggle( 'show' ) );
+Altis.Consent.closeUpdatedMessage.addEventListener( 'click', () => Altis.Consent.cookieUpdatedMessage.toggle( 'show' ) );
