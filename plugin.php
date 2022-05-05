7546e9ee64b762bda60198374f51d5a01111efdc
Robert O'Rourke <robert@o-rourke.org>
2022-05-05 13:27:47 +0100

    Bump version 1.0.15

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Altis Consent
  * Plugin URI: https://github.com/humanmade/altis-consent
  * Description: Hooks into the Consent API to provide basic settings and a cookie consent banner for Altis.
- * Version: 1.0.11
+ * Version: 1.0.15
  * Text Domain: altis-consent
  * Domain Path: /languages
  * Author: Human Made
